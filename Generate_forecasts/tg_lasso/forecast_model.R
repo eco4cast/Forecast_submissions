477237e8c301070266a31eae15300eeba52e0cb1
robbinscalebj <robbinscalebj@gmail.com>
2023-03-05 17:07:59 -0600

    blah

--- Generate_forecasts/tg_lasso/forecast_model.R
+++ Generate_forecasts/tg_lasso/forecast_model.R
@@ -11,7 +11,6 @@ library(lubridate)
 library(rMR)
 library(glue)
 library(decor)
-source(here("EFI_Theory/ignore_sigpipe.R")) #might fail locally
 library(tsibble)
 library(fable)
 library(arrow)
@@ -19,6 +18,7 @@ library(bundle)
 library(glmnet)
 here::i_am("EFI_Theory/Generate_forecasts/tg_lasso/forecast_model.R")
 source(here("EFI_Theory/download_target.R"))
+source(here("EFI_Theory/ignore_sigpipe.R")) #might fail locally
 
 
 
