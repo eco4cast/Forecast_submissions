01fd8cee1d5e1339468bf6ce5691e0d232c7e0cb
abbylewis <aslewis@vt.edu>
2023-09-20 14:34:32 -0400

    better structure for all lm automation/backfilling

---
+++ Generate_forecasts/humidity_lm/run_forecast.R
@@ -0,0 +1,8 @@
+source("./Generate_forecasts/humidity_lm/forecast_model.R")
+
+tryCatch({
+  generate_tg_forecast(forecast_date = Sys.Date(),
+                       forecast_model = forecast_model,
+                       model_themes = model_themes,
+                       model_id = model_id)
+}, error=function(e){cat("ERROR with forecast generation:\n",conditionMessage(e), "\n")})
