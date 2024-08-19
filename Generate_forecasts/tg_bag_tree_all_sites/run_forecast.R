a72fc721fab9a9a6dfeaef67367ce287e3976807
robbinscalebj <65026103+robbinscalebj@users.noreply.github.com>
2024-08-19 11:31:13 -0500

    bag_tree_all_sites updated to modular function workflow

---
+++ Generate_forecasts/tg_bag_tree_all_sites/run_forecast.R
@@ -0,0 +1,11 @@
+source("./Generate_forecasts/tg_bag_tree_all_sites/forecast_model.R")
+ 
+
+
+tryCatch({
+  generate_tg_forecast(forecast_date = Sys.Date(),
+                       forecast_model = forecast_model,
+                       model_themes = model_themes,
+                       model_id = model_id,
+                       all_sites = T)
+}, error=function(e){cat("ERROR with forecast generation:\n",conditionMessage(e), "\n")})
